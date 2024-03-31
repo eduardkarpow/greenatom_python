8ada2ccf35bbf520eba45f1821dd9a925c1fac2a
eduardkarpow <67784926+eduardkarpow@users.noreply.github.com>
2024-03-31 20:49:04 +0300

    added db fixed robot actions added endpoint for log data

--- main.py
+++ main.py
@@ -1,9 +1,44 @@
+from datetime import datetime
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 import os
+import sqlite3
+
+CREATE_TABLE_SCRIPT = "CREATE TABLE IF NOT EXISTS logs(" \
+                      "_id INTEGER PRIMARY KEY," \
+                      "start DATETIME NOT NULL," \
+                      "end DATETIME," \
+                      "start_index int NOT NULL" \
+                      ");"
+curId = None
+connection = sqlite3.connect("logs.db")
+cursor = connection.cursor()
+cursor.execute(CREATE_TABLE_SCRIPT)
+
+
+def insertScript(startIndex: int):
+    global curId
+    sql = "INSERT INTO logs (start, start_index) VALUES ('{}', {});".format(datetime.now(), startIndex)
+    cursor.execute(sql)
+    connection.commit()
+    getIdx = "SELECT _id FROM logs ORDER BY _id DESC LIMIT 1;"
+    res = cursor.execute(getIdx)
+    curId = res.fetchone()[0]
+
+
+def updateScript():
+    sql = "UPDATE logs SET end = '{}' WHERE _id = {};".format(datetime.now(), curId)
+    cursor.execute(sql)
+    connection.commit()
+
+
+def prepareData(el: list):
+    dct = {"id": el[0], "start_datetime": el[1], "end_datetime": el[2], "start_index": el[3]}
+    return dct
 
 stopFlag = 1
 
@@ -20,6 +55,7 @@ class StartModel(BaseModel):
 @app.get("/stop")
 async def stop():
     global stopFlag
+    updateScript()
     stopFlag = 1
 
 
@@ -27,13 +63,21 @@ async def stop():
 async def start(startModel: StartModel):
     global stopFlag
     stopFlag = 0
+    insertScript(startModel.startN)
     os.system("start cmd /k python robot.py " + str(startModel.startN))
     return JSONResponse(content=jsonable_encoder({"message": "success"}))
+
 
 @app.get("/isStopped")
 async def isStopped():
     return JSONResponse(content=jsonable_encoder({"stopFlag": stopFlag}))
 
+
+@app.get("/getLogs")
+async def getLogs():
+    sql = "SELECT * FROM logs;"
+    res = cursor.execute(sql)
+    return JSONResponse(content=jsonable_encoder(list(map(prepareData, res.fetchall()))))
 if __name__ == '__main__':
     print("Working")
 
--- robot.py
+++ robot.py
@@ -1,19 +1,28 @@
 import time
 import argparse
 import requests
-
+import asyncio
+import threading
 
 BASE_URL = "http://localhost:8000"
+stopFlag = 0
 
-def work(start: int):
+def req():
+    global stopFlag
+    r = requests.get(BASE_URL + "/isStopped", headers={'Accept': 'application/json'}).json()
+    stopFlag = r["stopFlag"]
+    return
+
+
+async def work(start: int):
     t = time.time()
     n = start
     while True:
         if time.time() - t >= 1.0:
             t = time.time()
 
-            r = requests.get(BASE_URL+"/isStopped", headers={'Accept': 'application/json'}).json()
-            if r['stopFlag'] == 1:
+            threading.Thread(target=req, args=()).start()
+            if stopFlag:
                 return
             print(n)
             n += 1
@@ -22,5 +31,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Задание стартового значния")
     parser.add_argument("start", help="стартовое знаачение")
     args = parser.parse_args()
-    work(int(args.start))
+    asyncio.run(work(int(args.start)))
+
 
